9de0d4934a5ceb7dad9cc4ea95768247beefafcb
Srabanti Chakraborty <67810931+srabantic@users.noreply.github.com>
2024-04-25 15:17:30 -0400

    Add an example of the expected return

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -5,9 +5,11 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
-Input: s = "A man, a plan, a canal: Panama"
-Output: true
-Explanation: "amanaplanacanalpanama" is a palindrome.
+Example:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 """
 def maxProfit(self, prices: List[int]) -> int:
         buy = prices[0]
