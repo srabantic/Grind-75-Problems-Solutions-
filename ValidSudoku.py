4e49ef258bb5d09a188545754a940a1f3f12af55
Srabanti Chakraborty <srabantic2013@gmail.com>
2025-08-04 23:42:45 -0600

    Implement LC Problem Valid Sudoku

---
+++ ValidSudoku.py
@@ -0,0 +1,88 @@
+from typing import DefaultDict, List
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Notes:
+    Time Complexity : O(n^2)
+    Space Complexity : O(n^2)
+"""
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows =  DefaultDict(set)
+        cols = DefaultDict(set)
+        squares = DefaultDict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if (board[r][c] != "."):
+                    if(board[r][c] in rows[r] or 
+                       board[r][c] in cols[c] or 
+                       board[r][c] in squares[(r // 3, c // 3)]):
+                        return False 
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    squares[(r//3, c // 3)].add(board[r][c])
+
+        return True 
+
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+solution = Solution()
+print(solution.isValidSudoku(board))   
+
+board1 = [["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+solution = Solution()
+print(solution.isValidSudoku(board1))
